068fef61565cb06ccbe1ece7bda6aa65f602f422
Andrea <bragg003@gmail.com>
2024-06-02 00:02:32 +0200

    implemented some nice plugins and keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- New tab
+keymap.set("n", "te", ":tabedit ")
+
+-- telescope with space t
+keymap.set("n", "<leader>t", ":Telescope ")
+
+-- remove recording q
+keymap.set("n", "q", "<Nop>")
+
+-- insert blank line with CR
+keymap.set("n", "<CR>", '@="m`o<C-V><Esc>``"<CR>')
+keymap.set("n", "<S-CR>", '@="m`O<C-V><Esc>``"<CR>')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,47 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+	{
+		"folke/flash.nvim",
+		enabled = false,
+	},
+	{
+		-- animations
+		"echasnovski/mini.animate",
+		event = "VeryLazy",
+		opts = function(_, opts)
+			opts.scroll = {
+				enable = false,
+			}
+		end,
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		opts = function(_, opts)
+			local logo = [[
+██╗  ██╗███████╗██╗     ██╗      ██████╗     ██╗  ██╗██╗████████╗████████╗██╗   ██╗
+██║  ██║██╔════╝██║     ██║     ██╔═══██╗    ██║ ██╔╝██║╚══██╔══╝╚══██╔══╝╚██╗ ██╔╝
+███████║█████╗  ██║     ██║     ██║   ██║    █████╔╝ ██║   ██║      ██║    ╚████╔╝ 
+██╔══██║██╔══╝  ██║     ██║     ██║   ██║    ██╔═██╗ ██║   ██║      ██║     ╚██╔╝  
+██║  ██║███████╗███████╗███████╗╚██████╔╝    ██║  ██╗██║   ██║      ██║      ██║   
+╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝     ╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝      ╚═╝   
+                                                    ⠀⠀⠀⠀⢀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⠾⠛⠶⣄⢀⣠⣤⠴⢦⡀⠀⠀⠀⠀
+                                                    ⠀⠀⠀⢠⡿⠉⠉⠉⠛⠶⠶⠖⠒⠒⣾⠋⠀⢀⣀⣙⣯⡁⠀⠀⠀⣿⠀⠀⠀⠀
+                                                    ⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⢯⣼⠋⠉⠙⢶⠞⠛⠻⣆⠀⠀⠀
+                                                    ⠀⠀⠀⢸⣧⠆⠀⠀⠀⠀⠀⠀⠀⠀⠻⣦⣤⡤⢿⡀⠀⢀⣼⣷⠀⠀⣽⠀⠀⠀
+                                                    ⠀⠀⠀⣼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⢏⡉⠁⣠⡾⣇⠀⠀⠀
+                                                    ⠀⠀⢰⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠋⠉⠀⢻⡀⠀⠀
+                                                    ⣀⣠⣼⣧⣤⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠐⠖⢻⡟⠓⠒
+                                                    ⠀⠀⠈⣷⣀⡀⠀⠘⠿⠇⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠿⠟⠀⠀⠀⠲⣾⠦⢤⠀
+                                                    ⠀⠀⠋⠙⣧⣀⡀⠀⠀⠀⠀⠀⠀⠘⠦⠼⠃⠀⠀⠀⠀⠀⠀⠀⢤⣼⣏⠀⠀⠀
+                                                    ⠀⠀⢀⠴⠚⠻⢧⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠞⠉⠉⠓⠀⠀
+                                                    ⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠶⠶⠶⣶⣤⣴⡶⠶⠶⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀
+    ]]
+
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+			opts.config.header = vim.split(logo, "\n")
+		end,
+	},
+}
