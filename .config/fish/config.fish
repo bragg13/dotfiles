316c74bc9bc94cfacbe940267b3721abdb451b52
Andrea Bragante <a.bragante@primopositum.com>
2024-09-19 14:30:05 +0200

    slightly edited fish and tmux conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,9 @@
 set fish_greeting ""
 
-if type -q eza
-    alias ll "eza -l -g --icons"
-    alias lla "ll -a"
-    alias ld "ll -D"
-    alias ls eza
-    alias llt "eza -T -L 2 -l -g --icons"
-end
+alias ll "eza -l -g --icons"
+alias lla "ll -a"
+alias ld "ll -D"
+alias ls eza
+alias llt "eza -T -L 2 -l -g --icons"
 
 alias dotconfig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
