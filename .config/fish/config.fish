77cea4354da2bc60465cbab256377c67e446fa88
Andrea Bragante <andrea@Mac.lan>
2024-09-30 18:35:00 +0200

    osx fish installation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,13 @@ if type -q eza
     alias ld "ll -D"
     alias ls eza
     alias llt "eza -T -L 2 -l -g --icons"
+
 end
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Setting PATH for Python 3.12
+# The original version is saved in /Users/andrea/.config/fish/conf$
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bi$
 
 alias dotconfig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
